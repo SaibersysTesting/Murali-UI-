e4c33af851750f1cf828a298b81473838ceab2dd
arun teja <training.saibersys9@gmail.com>
2016-09-19 14:08:46 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,3 @@
+function myfun(){
+alert("Hello html");
+}
---
+++ myscript1.js
@@ -0,0 +1,8 @@
+	
+	function mydate(){document.write("hello murali");
+	document.write("<h1>this is a heading</h1>");
+	//This is a code to dispaly date
+	document.write("<p>"+Date()+"</p>");
+	
+	
+	}
