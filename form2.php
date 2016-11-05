c331a13f5b775cb68cb2341723ac7c49424d0fa2
arun teja <training.saibersys9@gmail.com>
2016-11-05 17:03:03 -0500

    Add files via upload

---
+++ form2.php
@@ -0,0 +1,12 @@
+<html>
+<head></head>
+<body>
+
+Welcome <?php echo $_POST["fname"]; ?><br>
+Your first name: <?php echo $_POST["fname"]; ?><br>
+  last name: <?php echo $_POST["lname"]; ?><br>
+  country :<?php echo $_POST["Country"]; ?><br>
+  consultancy :<?php echo $_POST["consulname"]; ?><br>
+    roomno  <?php echo $_POST["roomno"]; ?><br>
+</body>
+</html>
